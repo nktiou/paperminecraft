5a7b22b2e19db62b120d0cae37c4ba88c157f748
nktiou <bigbongzhongli@yandex.com>
2021-06-02 00:45:58 -0400

    Added ore generation

--- terrain.py
+++ terrain.py
@@ -3,8 +3,8 @@ pygame.init()
 
 WORLD_LENGTH = 24
 WORLD_HEIGHT = 16
-SEA_LEVEL = 25
-MEAN_HEIGHT = 7
+SEA_LEVEL = 12
+MEAN_HEIGHT = 10
 
 PLAINS = random.randint(-1,1)
 
@@ -16,19 +16,20 @@ clock = pygame.time.Clock()
 dirt = pygame.image.load('assets/dirt.png')
 dirt_grass = pygame.image.load('assets/dirt_grass.png')
 stone = pygame.image.load('assets/stone.png')
+coal = pygame.image.load('assets/coal.png')
 
 world = np.zeros((WORLD_LENGTH,WORLD_HEIGHT))
 #world = [[0]*WORLD_HEIGHT]*WORLD_LENGTH
 
 class worldGenerator():
     def __init__(self):
-        pass
+        self
 # 0 = air, 1 = dirt, 2 = grass, 3 = stone
     def generate():
         RandomHeight = MEAN_HEIGHT
         for x in range(WORLD_LENGTH):
             RandomHeight += random.randint(-1,1)
-            DirtHeight = random.randint(3,4)
+            DirtHeight = random.randint(2,4)
             for y in range(RandomHeight, WORLD_HEIGHT):
                 if y == RandomHeight:
                     world[x][y] = 2
@@ -38,7 +39,10 @@ class worldGenerator():
                     world[x][y] = 3
 
     def generateOre():
-        pass
+        for i in range(int(WORLD_LENGTH*1.5)):
+            RandomX, RandomY = random.randint(0,WORLD_LENGTH-1),random.randint(0,WORLD_HEIGHT-1)
+            if world[RandomX][RandomY] == 3:
+                world[RandomX][RandomY] = 4
 
     def draw():
         for x in range(WORLD_LENGTH):
@@ -49,9 +53,11 @@ class worldGenerator():
                     game.blit(dirt_grass,(x*48,y*48))
                 elif world[x][y] == 3:
                     game.blit(stone,(x*48,y*48))
-
+                elif world[x][y] == 4:
+                    game.blit(coal,(x*48,y*48))
 game.fill((102,255,255))
 worldGenerator.generate()
+worldGenerator.generateOre()
 worldGenerator.draw()
 
 run = True
